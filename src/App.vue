8671386fc4af6b0954b3489ac991d1cc6d24bcb0
GabrielMM182 <gmm_morais@hotmail.com>
2022-02-16 13:39:35 -0300

    consumindo api http

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
         <img :src="foto.url" :alt="foto.titulo" />
       </li>
     </ul>
-
   </div>
 </template>
 
@@ -16,19 +15,20 @@ export default {
   data() {
     return {
       titulo: "blink-182",
-      fotos: [
-        {
-          url:
-            "https://blognroll.com.br/wp-content/uploads/2020/11/blink-182-600x337.jpg",
-          titulo: "blinkao"
-        },
-        {
-          url:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Blink182.jpg/1200px-Blink182.jpg",
-          titulo: "brinkk"
-        }
-      ]
+      fotos: []
     };
+  },
+
+  created() {
+
+    this.$http.get('http://localhost:3000/v1/fotos')
+    .then(res => res.json())
+    .then(lista_fotos => this.fotos = lista_fotos)
+
   }
-};
+}
 </script>
+
+<style>
+
+</style>

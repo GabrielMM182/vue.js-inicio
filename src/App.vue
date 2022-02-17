f1f11e6c3d095edf0a40e7a426ae8e451769cc17
GabrielMM182 <gmm_morais@hotmail.com>
2022-02-17 14:27:38 -0300

    rotas aprimoradas e menu component criado

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="corpo">
-    <router-view></router-view>
+
+    <meu-menu :routes="routes"> </meu-menu>
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
 
+import {routes} from './routes'
+import Menu from './components/shared/menu/Menu.vue'
+
 export default {
+
+  components: {
+    'meu-menu' : Menu
+  },
+
+  data() {
+
+    return {
+
+      routes
+
+    }
+
+  }
 
 };
 </script>
@@ -16,6 +38,14 @@ export default {
   font-family: Arial, Helvetica, sans-serif;
   width: 96%;
   margin: 0 auto;
+}
+
+.pagina-fade-enter, .pagina-fade-leave-active {
+  opacity: 0
+}
+
+.pagina-fade-enter-active, .pagina-fade-leave-active {
+  transition: opacity .4s
 }
 
 </style>
---
+++ src/components/shared/menu/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li v-for="route in routes" :key="route.titulo">
+          <router-link :to="route.path ? route.path : '/'">
+            {{route.titulo}}
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['routes']
+  };
+</script>
+
+<style scoped></style>
